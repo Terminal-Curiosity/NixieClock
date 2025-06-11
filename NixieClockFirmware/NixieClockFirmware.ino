dfe2127fe4f7ee5cecd44571f76e61c146012840
Terminal-Curiosity <137018407+Terminal-Curiosity@users.noreply.github.com>
2025-06-11 22:54:13 +0930

    nixie panel PCB layout progress
    laid out tube 1 with top and bottom traces. added GND polygon on both sides, with exception zone around tube 1. tubes 2-4 to be laid out with top and bottom traces

--- NixieClockFirmware/NixieClockFirmware.ino
+++ NixieClockFirmware/NixieClockFirmware.ino
@@ -1,19 +1,21 @@
 #include <Wire.h>
 
 //define digital pins communicating to driver chip
-const int pin1 = 19;
-const int pin2 = 21;
-const int pin3 = 22;
-const int pin4 = 23;
+#define pin1 = 19
+#define pin2 = 1
+#define pin3 = 3
+#define pin4 = 23
 
-const int IO1_addr = 0x20; //assign the i2c expander addresses
+#define led_pin = 4
+
+#define IO1_addr 0x24 //assign the i2c expander addresses
 const int IO2_addr = 0x22;
-const int IO3_addr = 0x24;
+const int IO3_addr = 0x20;
 
-#define SDA_pin 17
-#define SCL_pin 16 
+#define SDA_pin 21
+#define SCL_pin 22
 
-const int digitCode[] = {8,4,3,2,10,1,11,12,9,0}; //working using non inverted outputs
+const int digitCode[] = {8,1,10,11,2,12,4,0,9,3}; //working using non inverted outputs
 //const int digitCode[] = {0b0111,0b1011,0b1100,0b1101,0b0101,0b1110,0b0100,0b0011,0b0110,0b1111};
 
 void digitCycle(int);
@@ -22,15 +24,17 @@ void write1byte(byte, byte);
 
 void setup() {
   // put your setup code here, to run once:
-  Wire.begin(SDA_pin, SCL_pin); // initialise IO bit expander 1 I2C channel
-  Serial.begin(115200);
-  pinMode(SDA_pin, INPUT_PULLUP); //use internal pullups on the I2c pins instead of external hardware resistors
-  pinMode(SCL_pin, INPUT_PULLUP);
+  Wire.begin(SDA_pin,SCL_pin); // initialise IO bit expander 1 I2C channel
+  Wire.setClock(100000);
+  //Serial.begin(115200);
+  //pinMode(SDA_pin, INPUT_PULLUP); //use internal pullups on the I2c pins instead of external hardware resistors
+  //pinMode(SCL_pin, INPUT_PULLUP);
   
   pinMode(pin1, OUTPUT); // set all digital pins to output
   pinMode(pin2, OUTPUT);
   pinMode(pin3, OUTPUT);
   pinMode(pin4, OUTPUT);
+  pinMode(led_pin, OUTPUT);
   
   digitalWrite(pin1,LOW); // initialise all digital pins low
   digitalWrite(pin2,LOW);
@@ -40,9 +44,11 @@ void setup() {
   Wire.beginTransmission(IO3_addr); // turn off all GPIO pins on port expanders by writing all "1"
   Wire.write(0xFF);
   Wire.endTransmission();
+  delay(5);
   Wire.beginTransmission(IO2_addr);
   Wire.write(0xFF);
   Wire.endTransmission();
+  delay(5);
   Wire.beginTransmission(IO1_addr);
   Wire.write(0xFF);
   Wire.endTransmission();
@@ -56,6 +62,8 @@ void loop() {
       digitCycle(1000);
       digitCycle(100);
       
+
+     
       
       
     }
@@ -67,7 +75,7 @@ void digitCycle(int delayms)
   for(int i=0; i<10; i++) //loop through all possible 4 bit states rapidly to show that we have reached end of the loop
   {
     writeDigit(i);
-    write1byte(IO3_addr,i);
+    write1byte(IO1_addr,i);
     delay(delayms); //wait 0.5sec before next transition
   }
 }
@@ -89,4 +97,10 @@ void write1byte(byte address, byte digit)
     //Wire.write(0x01);
     Wire.write(sendVal);
     Wire.endTransmission();
+    byte error = Wire.endTransmission();
+    Serial.print("Sent to 0x");
+    Serial.print(address, HEX);
+    Serial.print(" — Error: ");
+    Serial.println(error);
+    delay(5);
 }
